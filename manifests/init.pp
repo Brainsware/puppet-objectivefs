bd8682066f76fa08f6acc3ceaf7adc6b23f5c9f2
Igor Galić <i.galic@brainsware.org>
2016-04-10 00:22:46 +0200

    types have to be qualified

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,18 +4,18 @@
 # This holds together the installation and configuration of the system
 #
 class objectivefs (
-  String[9, 9]  $uid,
-  String        $version          = '4.1.1',
-  String        $package_provider = $::objectivefs::params::package_provider,
-  String        $package_name     = $::objectivefs::params::package_name,
-  Array[String] $dependencies     = $::objectivefs::params::dependencies,
-  Unixpath      $download_path    = $::objectivefs::params::download_path,
+  String[9, 9]        $uid,
+  String              $version          = '4.1.1',
+  String              $package_provider = $::objectivefs::params::package_provider,
+  String              $package_name     = $::objectivefs::params::package_name,
+  Array[String]       $dependencies     = $::objectivefs::params::dependencies,
+  Datatypes::Unixpath $download_path    = $::objectivefs::params::download_path,
   # These are "private" and you should only change them if you have your own mirror
-  String        $_base_url        = $::objectivefs::params::base_url,
-  String        $_package_prefix  = $::objectivefs::params::package_prefix,
-  String        $_package_suffix  = $::objectivefs::params::package_suffix,
-  String        $package_file     = "${_package_prefix}/${version}/${_package_suffix}",
-  HTTPSUrl      $download_url     = "${_base_url}/${uid}/${package_file}",
+  String              $_base_url        = $::objectivefs::params::base_url,
+  String              $_package_prefix  = $::objectivefs::params::package_prefix,
+  String              $_package_suffix  = $::objectivefs::params::package_suffix,
+  String              $package_file     = "${_package_prefix}/${version}/${_package_suffix}",
+  Datatypes::HTTPSUrl $download_url     = "${_base_url}/${uid}/${package_file}",
 ) inherits ::objectivefs::params {
 
   contain ::objectivefs::install
